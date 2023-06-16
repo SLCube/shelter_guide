c12ddd93d1c413dac2a91fbb91fcb70473a0144b
SLCube <kgm0096@naver.com>
2023-06-16 14:53:53 +0900

    Initial commit

---
+++ src/main/java/com/slcube/shelter_guide/ShelterGuideApplication.java
@@ -0,0 +1,13 @@
+package com.slcube.shelter_guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slcube/shelter_guide/ShelterGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slcube.shelter_guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
