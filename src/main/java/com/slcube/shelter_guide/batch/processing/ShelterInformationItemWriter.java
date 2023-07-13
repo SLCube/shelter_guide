8c0910f89fef4ec653e29949d41a66fd310706c2
SLCube <kgm0096@naver.com>
2023-07-13 18:56:38 +0900

    [feat] ItemWriter작성

--- src/main/java/com/slcube/shelter_guide/batch/processing/ShelterInformationItemWriter.java
+++ src/main/java/com/slcube/shelter_guide/batch/processing/ShelterInformationItemWriter.java
@@ -1,14 +1,19 @@
 package com.slcube.shelter_guide.batch.processing;
 
 import com.slcube.shelter_guide.batch.dto.SeoulShelterInformationResultDataDto;
+import com.slcube.shelter_guide.batch.service.ShelterInformationApiService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemWriter;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 public class ShelterInformationItemWriter implements ItemWriter<List<SeoulShelterInformationResultDataDto>> {
+
+    private final ShelterInformationApiService shelterInformationApiService;
 
     @Override
     public void write(List<? extends List<SeoulShelterInformationResultDataDto>> items) throws Exception {
-
+        items.forEach(shelterInformationApiService::storeShelterInformation);
     }
 }
