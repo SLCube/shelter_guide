85409ae2b2f081e526f5741faf084978e948b4f7
SLCube <kgm0096@naver.com>
2023-07-13 18:57:51 +0900

    [feat] 외부 api의 데이터검증 로직 추가

--- src/main/java/com/slcube/shelter_guide/batch/service/ShelterInformationApiService.java
+++ src/main/java/com/slcube/shelter_guide/batch/service/ShelterInformationApiService.java
@@ -2,28 +2,54 @@ package com.slcube.shelter_guide.batch.service;
 
 import com.slcube.shelter_guide.batch.dto.SeoulShelterInformationDto;
 import com.slcube.shelter_guide.batch.dto.SeoulShelterInformationResultDataDto;
+import com.slcube.shelter_guide.batch.dto.SeoulShelterInformationResultDto;
+import com.slcube.shelter_guide.batch.entity.ShelterInformationStaging;
+import com.slcube.shelter_guide.batch.mapper.ShelterInformationMapper;
+import com.slcube.shelter_guide.batch.repository.ShelterInformationStagingRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
+@Transactional(readOnly = true)
 public class ShelterInformationApiService {
 
     private final RestTemplate restTemplate;
+    private final ShelterInformationStagingRepository shelterInformationStagingRepository;
 
     public List<SeoulShelterInformationResultDataDto> fetchShelterInformation(int startIndex, int endIndex) {
-        String url = new StringBuilder().append("/")
-                .append(startIndex)
-                .append("/")
-                .append(endIndex)
-                .toString();
+        String url = "/";
 
-        return restTemplate.getForObject(url, SeoulShelterInformationDto.class).getResult().getResultDataDtoList();
+        URI uri = UriComponentsBuilder.fromHttpUrl(url)
+                .pathSegment(String.valueOf(startIndex), String.valueOf(endIndex))
+                .build()
+                .toUri();
+
+        try {
+            SeoulShelterInformationDto seoulShelterInformationDto = restTemplate.getForObject(uri, SeoulShelterInformationDto.class);
+
+            if (seoulShelterInformationDto != null) {
+                SeoulShelterInformationResultDto result = seoulShelterInformationDto.getResult();
+                if (result != null) {
+                    return result.getResultDataDtoList();
+                }
+            }
+        } catch (Exception e) {
+            log.error("Error while calling REST API : ", e);
+        }
+
+
+        return Collections.emptyList();
     }
 }
 
