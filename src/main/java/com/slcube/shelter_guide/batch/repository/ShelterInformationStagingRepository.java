4568278abf5a8b2938f1cc5d820fecf44c6d1067
SLCube <kgm0096@naver.com>
2023-07-18 23:40:58 +0900

    [feat] 배치 결과를 저장하는 Entity의 기존의 데이터를 관리번호로 조회하는 jpa repospository의 method추가

--- src/main/java/com/slcube/shelter_guide/batch/repository/ShelterInformationStagingRepository.java
+++ src/main/java/com/slcube/shelter_guide/batch/repository/ShelterInformationStagingRepository.java
@@ -3,5 +3,8 @@ package com.slcube.shelter_guide.batch.repository;
 import com.slcube.shelter_guide.batch.entity.ShelterInformationStaging;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ShelterInformationStagingRepository extends JpaRepository<ShelterInformationStaging, Long> {
+    Optional<ShelterInformationStaging> findByManagementNumber(String managementNumber);
 }
