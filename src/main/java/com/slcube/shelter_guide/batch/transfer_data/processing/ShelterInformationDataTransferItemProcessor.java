e338d1a1b2dc299e0b9946b5dbd3864dd88c16ff
SLCube <kgm0096@naver.com>
2023-07-27 12:31:25 +0900

    [feat] 첫번째 배치가 실행된후 저장용 테이블에서 운영용 테이블로 데이터를 이관하는 배치를 호출하는 리스너 등록

---
+++ src/main/java/com/slcube/shelter_guide/batch/transfer_data/processing/ShelterInformationDataTransferItemProcessor.java
@@ -0,0 +1,40 @@
+package com.slcube.shelter_guide.batch.transfer_data.processing;
+
+import com.slcube.shelter_guide.batch.external_api.entity.ShelterInformationStaging;
+import com.slcube.shelter_guide.batch.transfer_data.comparator.ShelterInformationComparator;
+import com.slcube.shelter_guide.batch.transfer_data.service.ShelterInformationDataTransferService;
+import com.slcube.shelter_guide.business.entity.ShelterInformation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ShelterInformationDataTransferItemProcessor implements ItemProcessor<List<ShelterInformationStaging>, List<ShelterInformationStaging>> {
+
+    private final ShelterInformationDataTransferService shelterInformationDataTransferService;
+
+    @Override
+    public List<ShelterInformationStaging> process(List<ShelterInformationStaging> item) throws Exception {
+        Iterator<ShelterInformationStaging> iterator = item.iterator();
+        while (iterator.hasNext()) {
+            log.info("### execution event item reader!!!");
+            ShelterInformationStaging shelterInformationStaging = iterator.next();
+            Optional<ShelterInformation> shelterInformation = shelterInformationDataTransferService.findByManagementNumber(shelterInformationStaging.getManagementNumber());
+
+            if (shelterInformation.isPresent()) {
+                ShelterInformation shelterInformationEntity = shelterInformation.get();
+                if (!ShelterInformationComparator.areEqual(shelterInformationStaging, shelterInformationEntity)) {
+
+                }
+
+                iterator.remove();
+            }
+        }
+        return item;
+    }
+}
