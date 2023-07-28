2f43fb5e50ba36be4b8f722fb33798c3f24d98c1
SLCube <kgm0096@naver.com>
2023-07-28 11:36:07 +0900

    [test] 데이터 이관용 batch writer unit test 작성

---
+++ src/test/java/com/slcube/shelter_guide/batch/transfer_data/processing/ShelterInformationDataTransferItemWriterTest.java
@@ -0,0 +1,26 @@
+package com.slcube.shelter_guide.batch.transfer_data.processing;
+
+import com.slcube.shelter_guide.batch.transfer_data.service.ShelterInformationDataTransferService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ShelterInformationDataTransferItemWriterTest {
+
+    @Mock
+    private ShelterInformationDataTransferService shelterInformationDataTransferService;
+
+    @InjectMocks
+    private ShelterInformationDataTransferItemWriter shelterInformationDataTransferItemWriter;
+
+    @Test
+    void list가_null일때_예외체크_테스트() {
+        Assertions.assertThatThrownBy(() -> shelterInformationDataTransferItemWriter.write(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Items can not be null");
+    }
+}
