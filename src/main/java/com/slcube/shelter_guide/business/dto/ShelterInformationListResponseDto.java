2b941012af6214b060cc4fbabfc0e6eacfb52ca2
SLCube <kgm0096@naver.com>
2023-08-01 16:45:35 +0900

    [feat] 임시로 사용할 api response dto 정의

---
+++ src/main/java/com/slcube/shelter_guide/business/dto/ShelterInformationListResponseDto.java
@@ -0,0 +1,44 @@
+package com.slcube.shelter_guide.business.dto;
+
+import com.slcube.shelter_guide.business.entity.ShelterInformation;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ShelterInformationListResponseDto {
+
+    private Long shelterInformationId;
+    private String managementNumber;
+    private String licenseDate;
+    private String businessStatusCode;
+    private String businessStatusName;
+    private String detailedBusinessStatusCode;
+    private String detailedBusinessStatusName;
+    private String closingDate;
+    private Double positionX;
+    private Double positionY;
+    private Double locationArea;
+    private String locationPostalCode;
+    private String landNumberAddress;
+    private String roadNameAddress;
+    private String roadNamePostalCode;
+
+    public ShelterInformationListResponseDto(ShelterInformation shelterInformation) {
+        this.shelterInformationId = shelterInformation.getId();
+        this.managementNumber = shelterInformation.getManagementNumber();
+        this.licenseDate = shelterInformation.getLicenseDate();
+        this.businessStatusCode = shelterInformation.getBusinessStatusCode();
+        this.businessStatusName = shelterInformation.getBusinessStatusName();
+        this.detailedBusinessStatusCode = shelterInformation.getDetailedBusinessStatusCode();
+        this.detailedBusinessStatusName = shelterInformation.getDetailedBusinessStatusName();
+        this.closingDate = shelterInformation.getClosingDate();
+        this.positionX = shelterInformation.getPositionX();
+        this.positionY = shelterInformation.getPositionY();
+        this.locationArea = shelterInformation.getLocationArea();
+        this.locationPostalCode = shelterInformation.getLocationPostalCode();
+        this.landNumberAddress = shelterInformation.getLandNumberAddress();
+        this.roadNameAddress = shelterInformation.getRoadNameAddress();
+        this.roadNamePostalCode = shelterInformation.getRoadNamePostalCode();
+    }
+}
