2f1db4aeeeb273581a3f10c5803f3cfff48dd9db
강경모 <kgm0096@naver.com>
2024-06-27 11:12:36 +0900

    [feat] 프로그램 실행을 위한 main method 추가

---
+++ module-api/module-client-api/src/main/java/ShelterGuideApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class ShelterGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterGuideApplication.class, args);
+    }
+}
