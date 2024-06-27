2a6d9477730cf04febc618641113b49ef17061a2
강경모 <kgm0096@naver.com>
2024-06-27 11:22:04 +0900

    [feat] job parameters null체크 로직 추가

--- module-batch/src/main/java/batch/config/VersionJobParametersIncrementer.java
+++ module-batch/src/main/java/batch/config/VersionJobParametersIncrementer.java
@@ -3,12 +3,14 @@ package batch.config;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersIncrementer;
+import org.springframework.util.Assert;
 
 import java.util.HashMap;
 
 public class VersionJobParametersIncrementer implements JobParametersIncrementer {
     @Override
     public JobParameters getNext(JobParameters parameters) {
+        Assert.notNull(parameters, "parameters can not be null");
         HashMap<String, JobParameter> params = new HashMap<>(parameters.getParameters());
         params.put("version", new JobParameter(System.currentTimeMillis()));
         return new JobParameters(params);
